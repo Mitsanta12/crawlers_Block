e6eb3a604e3d01b41e13cf5c411fe84b8b88820c
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2022-06-03 22:52:33 +0300

    fix 'crawlers.sh'

--- crawlersBlock.sh
+++ crawlersBlock.sh
@@ -3,12 +3,14 @@
 #Mettre en backup les donnees d'access_log
 cp --backup /var/log/apache2/access.log
 
-#verifier chaque log qui utilise des cron
+#verificatio de chaque log qui utilise des cron
 cd /var/log/apache2/
+
 cat access.log
 do crontab -l; 
-#Recuperer tous les adresses clients renvoyant 404(qui ont rencontré des scrawlers)(
+#Recuperation de tous les adresses clients qui renvoye des codes 404(c'est à dire qui ont rencontré des scrawlers)(
 do
-#Script du bloquage de scrawlers
+
+#Script du bloquage de scrawlers:
 awk '($9 ~ /404/)' /var/log/apache2/access.log | awk '{print $9,$7}' | sort | sed -n '/404$/p'|\ awk '{print $1} |tail -f |iptables -A INPUT -s $1 -j DROP  ;
 done
